66858b5b789af0e5ea794d0ac7f8054d4a7796df
axl <axlceb@gmail.com>
2022-04-25 19:40:15 +0200

    Update Connect.java
    Add Create Table, Insert data, select data

--- src/main/java/org/example/Connect.java
+++ src/main/java/org/example/Connect.java
@@ -5,6 +5,7 @@ import java.sql.*;
 public class Connect {
     private Connection conn;
     private Statement st;
+    ResultSet rs;
 
     public Connect() {
         try {
@@ -15,7 +16,17 @@ public class Connect {
         try {
             conn = DriverManager.getConnection("jdbc:h2:~/test7", "sa", "");
             System.out.println("Status: Connected!");
+
             st = conn.createStatement();
+            st.execute("CREATE TABLE IF NOT EXISTS TEST (ID INT PRIMARY KEY, TEXT VARCHAR(64));");
+            st.execute("MERGE INTO TEST (ID, TEXT) VALUES (1, 'text text text');");
+            rs = st.executeQuery("SELECT * FROM TEST;");
+
+            rs.next();
+            Integer id = rs.getInt("ID");
+            String text = rs.getString("TEXT");
+            System.out.println("ID = " + id + ", TEXT = " + text);
+
             conn.close();
             System.out.println("Status: Connection Closed!");
         } catch (SQLException e) {
